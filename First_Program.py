134c5102e617690d98261adef2092cde863fe326
franekjoniec <franekj12@yahoo.com>
2022-02-03 22:14:09 -0800

    Adding my first program
    Adding my first program

---
+++ First_Program.py
@@ -0,0 +1,27 @@
+first_name = "Frank"
+last_name = "Joniec"
+full_name = first_name +" "+ last_name
+#print("Hello "+full_name)
+#print(type(first_name))
+
+age = 22
+#age += 1
+#print("My age is: "+str(age))
+#print(type(age))
+
+height = 5.8
+#print("My height is: "+str(height)+"cm")
+#print(type(height))
+
+human = True
+#print("Am I a human? "+str(human))
+#print(type(human))
+
+
+
+
+
+print(((("Hello I am "+full_name)+(", My age is: "+str(age)))+(", My height is: "+str(height)+"cm"))+(", Am I a human? "+str(human)))
+#print("My age is: "+str(age))
+#print("My height is: "+str(height)+"cm")
+#print("Am I a human? "+str(human))
